6a06bf15de588fb6b11f4ec00fadf362dfda2765
theQuery <tomkri2000@outlook.com>
2023-01-12 22:23:57 +0100

    Alter glow to be below all in the portal component

--- src/components/Portal.jsx
+++ src/components/Portal.jsx
@@ -5,13 +5,13 @@ function Portal({ text }) {
     // Use this for the click sound effect:
     // https://www.youtube.com/watch?v=l3JXzuX7mmA
     return <div className='portal'>
+        <div className='portal__glow'></div>
         <img
             className='portal__image'
             src={portal}
             alt={text}
             draggable={false}
         />
-        <div className='portal__glow'></div>
         <p className='portal__text'>
             {text}
         </p>
