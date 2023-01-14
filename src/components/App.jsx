3e587c662d91133e732832e6de5ff06c2f4b7576
theQuery <tomkri2000@outlook.com>
2023-01-14 01:19:28 +0100

    Add Blur and Footer to App

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,9 +1,13 @@
 import '../styles/App.css';
 import HomePage from './HomePage';
+import Blur from './Blur';
+import Footer from './Footer';
 
 function App() {
   return <main className='app'>
     <HomePage />
+    <Blur />
+    <Footer />
   </main>
 }
 
