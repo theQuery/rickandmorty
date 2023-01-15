d6344befb205fd2e243f559023eb89ec44c1b9b2
theQuery <tomkri2000@outlook.com>
2023-01-15 08:30:08 +0100

    Add portal next to root for Blur component

--- src/components/Blur.jsx
+++ src/components/Blur.jsx
@@ -1,10 +1,15 @@
 import '../styles/Blur.css';
+import ReactDOM from 'react-dom';
 import { useBlur } from '../contexts/BlurProvider';
 
 function Blur() {
     const [isBlurred] = useBlur();
     const className = isBlurred ? 'blur__enabled' : 'blur';
-    return <div className={className}></div>;
+
+    return ReactDOM.createPortal(
+        <div className={className}></div>,
+        document.getElementById('portal')
+    );
 }
 
 export default Blur;
