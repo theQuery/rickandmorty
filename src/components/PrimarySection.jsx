16fb0c5935b13167ec5a8613c126133652c94d26
theQuery <tomkri2000@outlook.com>
2023-01-16 20:56:23 +0100

    Remove animated svg to increase performance

--- src/components/PrimarySection.jsx
+++ src/components/PrimarySection.jsx
@@ -7,7 +7,7 @@ function PrimarySection({ image }) {
       style={{ backgroundImage: `url(${image})` }}
     >
     </div>
-    <div className='primary-section__footer'></div>
+    <div className='primary-section__cover'></div>
   </section>
 }
 
