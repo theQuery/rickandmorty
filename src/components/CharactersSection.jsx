a46a7d6a065d7e9601be1756297e80d4f8e3103c
theQuery <tomkri2000@outlook.com>
2023-01-20 17:41:02 +0100

    Add CharactersSection component

---
+++ src/components/CharactersSection.jsx
@@ -0,0 +1,9 @@
+import '../styles/CharactersSection.css';
+
+function CharactersSection() {
+  return <section className='characters-section'>
+    <div className='characters-section__image'></div>
+  </section>
+}
+
+export default CharactersSection;
