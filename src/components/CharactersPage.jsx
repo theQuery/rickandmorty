0a29610ec87861b99b27f92bc6415440fe92fc40
theQuery <tomkri2000@outlook.com>
2023-01-20 17:42:41 +0100

    Add usage of SectionScroller and CharactersSection

--- src/components/CharactersPage.jsx
+++ src/components/CharactersPage.jsx
@@ -1,14 +1,16 @@
 import '../styles/CharactersPage.css';
 import background2 from '../assets/background2.jpg';
 import PrimarySection from './PrimarySection';
-import SecondarySection from './SecondarySection';
+import CharactersSection from './CharactersSection';
 import NavBar from './NavBar';
+import SectionScroller from './SectionScroller';
 
 function CharactersPage() {
   return <main className='characters-page'>
     <PrimarySection image={background2} />
-    <SecondarySection />
+    <CharactersSection />
     <NavBar />
+    <SectionScroller />
   </main>
 }
 
