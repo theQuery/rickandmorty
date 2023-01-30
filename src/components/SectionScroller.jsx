b2a2834fa41d555f626a4934f56ea40c9372aa8a
theQuery <tomkri2000@outlook.com>
2023-01-30 22:56:14 +0100

    Alter styling/add more buttons in SectionScroller

--- src/components/SectionScroller.jsx
+++ src/components/SectionScroller.jsx
@@ -1,26 +1,30 @@
 import '../styles/SectionScroller.css';
-import useScrollPosition from '../hooks/useScrollPosition';
 import { useState, useEffect } from 'react';
-
-//? Maybe turn this into a parent component.
-//? Then get the direct children and use
-//? them as the sections? Also, use useRef().
-
-//* Maybe make the SCROLL button fill with glowing
-//* white as a progress bar so the user knows it.
+import useScrollPosition from '../hooks/useScrollPosition';
+import { ReactComponent as HomeIcon } from '../assets/home.svg';
+import { ReactComponent as PreviousIcon } from '../assets/previous.svg';
+import { ReactComponent as UpIcon } from '../assets/up.svg';
+import { ReactComponent as DownIcon } from '../assets/down.svg';
+import { ReactComponent as NextIcon } from '../assets/next.svg';
+import { ReactComponent as BaseIcon } from '../assets/base.svg';
 
 function SectionScroller() {
   const [mainElementHeight, setMainElementHeight] = useState(0);
   const [windowHeight, setWindowHeight] = useState(0);
   const [isAtHomeSection, setIsAtHomeSection] = useState(true);
   const [isAtBaseSection, setIsAtBaseSection] = useState(false);
+  const [isAtBottom, setIsAtBottom] = useState(false);
   const scrollPosition = useScrollPosition();
+  const scrollPositionAtBottom = mainElementHeight - windowHeight - 1;
 
   useEffect(() => {
     const mainElement = document.getElementsByTagName('main').item(0);
     const newMainElementHeight = mainElement.clientHeight;
-    const newWindowHeight = window.innerHeight;
     setMainElementHeight(newMainElementHeight);
+
+    if (!mainElementHeight) return;
+
+    const newWindowHeight = window.innerHeight;
     setWindowHeight(newWindowHeight);
 
     const sections = getSections();
@@ -28,12 +32,15 @@ function SectionScroller() {
     const newIsAtBaseSection = getIsAtBaseSection(sections);
     setIsAtHomeSection(newIsAtHomeSection);
     setIsAtBaseSection(newIsAtBaseSection);
+
+    const newIsAtBottom = getIsAtBottom();
+    setIsAtBottom(newIsAtBottom);
   }, [scrollPosition]);
 
   const getIsAtHomeSection = () => {
     const newIsAtHomeSection = scrollPosition === 0;
     return newIsAtHomeSection;
-  }
+  };
 
   const getIsAtBaseSection = sections => {
     const sectionsHeight = sections.reduce((accumulated, section) => {
@@ -42,11 +49,20 @@ function SectionScroller() {
 
     const scrollPositionInt = Math.ceil(scrollPosition);
     const baseSection = getBaseSection(sections);
-    const sectionsFinalHeight = sectionsHeight - baseSection.height;
+    const sectionsHeightTop = sectionsHeight - baseSection.height;
 
-    const newIsAtBaseSection = scrollPositionInt >= sectionsFinalHeight;
+    const newIsAtBaseSectionTop = scrollPositionInt >= sectionsHeightTop;
+    const newIsAtBottom = getIsAtBottom();
+
+    const newIsAtBaseSection = newIsAtBaseSectionTop || newIsAtBottom;
     return newIsAtBaseSection;
-  }
+  };
+
+  const getIsAtBottom = () => {
+    const scrollPositionInt = Math.ceil(scrollPosition);
+    const newIsAtBottom = scrollPositionInt >= scrollPositionAtBottom;
+    return newIsAtBottom;
+  };
 
   const getSections = () => {
     const sections = [];
@@ -59,15 +75,15 @@ function SectionScroller() {
     });
 
     return sections;
-  }
+  };
 
   const getHomeSection = sections => {
     return sections[0];
-  }
+  };
 
   const getBaseSection = sections => {
     return sections[sections.length - 1];
-  }
+  };
 
   const getNextSection = sections => {
     let nextSection, accumulated = 0;
@@ -85,7 +101,7 @@ function SectionScroller() {
 
     nextSection ||= getHomeSection(sections);
     return nextSection;
-  }
+  };
 
   const getPreviousSection = sections => {
     let previousSection, accumulated = 0;
@@ -104,35 +120,53 @@ function SectionScroller() {
 
     previousSection ||= getBaseSection(sections);
     return previousSection;
-  }
+  };
 
   const scrollSectionIntoView = section => {
     const sectionElement = document
       .getElementsByClassName(section.name).item(0);
 
     sectionElement.scrollIntoView({ behavior: 'smooth' });
-  }
+  };
+
+  const scrollUp = () => {
+    const newScrollPosition = scrollPosition - 200;
+    window.scrollTo(0, newScrollPosition);
+  };
+
+  const scrollDown = () => {
+    const newScrollPosition = scrollPosition + 200;
+    window.scrollTo(0, newScrollPosition);
+  };
 
   const handleClick = type => {
     const sections = getSections();
     let section;
 
     switch (type) {
+      case 'UP': return scrollUp();
+      case 'DOWN': return scrollDown();
       case 'NEXT': section = getNextSection(sections); break;
-      case 'BACK': section = getPreviousSection(sections); break;
+      case 'PREVIOUS': section = getPreviousSection(sections); break;
       case 'HOME': section = getHomeSection(sections); break;
       case 'BASE': section = getBaseSection(sections); break;
     }
 
     scrollSectionIntoView(section);
-  }
+  };
 
-  return <div className='scroll'>
-    <button disabled={isAtHomeSection} onClick={() => handleClick('HOME')}>HOME</button>
-    <button disabled={isAtHomeSection} onClick={() => handleClick('BACK')}>BACK</button>
-    <progress value={scrollPosition} max={mainElementHeight - windowHeight}></progress>
-    <button disabled={isAtBaseSection} onClick={() => handleClick('NEXT')}>NEXT</button>
-    <button disabled={isAtBaseSection} onClick={() => handleClick('BASE')}>BASE</button>
+  return <div className='section-scroller'>
+    <div>
+      <button disabled={isAtHomeSection} onClick={() => handleClick('HOME')}><HomeIcon /></button>
+      <button disabled={isAtHomeSection} onClick={() => handleClick('PREVIOUS')}><PreviousIcon /></button>
+      <button disabled={isAtHomeSection} onClick={() => handleClick('UP')}><UpIcon /></button>
+    </div>
+    <progress value={scrollPosition} max={scrollPositionAtBottom}></progress>
+    <div>
+      <button disabled={isAtBaseSection} onClick={() => handleClick('BASE')}><BaseIcon /></button>
+      <button disabled={isAtBaseSection} onClick={() => handleClick('NEXT')}><NextIcon /></button>
+      <button disabled={isAtBottom} onClick={() => handleClick('DOWN')}><DownIcon /></button>
+    </div>
   </div>
 }
 
